66264c0bade2099887be899ca5ef883a5e5d0334
LLczff <theeradon.dev@gmail.com>
2025-05-20 14:13:26 +0700

    update: not found

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 function NotFound() {
   return (
-    <main className="main-container items-center gap-2">
+    <main className="main-container items-center justify-center bg-white text-primary gap-2">
       <h1 className="text-6xl font-extrabold">404</h1>
       <h2 className="text-xl font-medium mb-3">Look like you're lost.</h2>
-      <Link href="/" className="btn-primary">
-        Back to Home
+      <Link href="/" className="p-3 text-white bg-primary rounded-lg">
+        Back to Start
       </Link>
     </main>
   );
